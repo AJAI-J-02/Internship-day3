d48fe4c67f6ab85864714465a8e3a45399fce086
GItconss <forgithub40@gmail.com>
2025-12-24 09:30:42 +0530

    initial angular project commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Service } from './pages/service/service';
+import { Contact } from './pages/contact/contact';
+import { Cyber } from './pages/cyber/cyber';
+import { Devops } from './pages/devops/devops';
+import { Cloud } from './pages/cloud/cloud';
+import { Dataengineering } from './pages/dataengineering/dataengineering';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:Home},
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+    {path:'about',component:About},
+    {path:'service',component:Service},
+    {path:'contact',component:Contact},
+    {path:'cyber',component:Cyber},
+    {path:'devops',component:Devops},
+    {path:'cloud',component:Cloud},
+    {path:'dataengineering',component:Dataengineering},
+
+
+
+];
